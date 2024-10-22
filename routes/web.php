59a43f16a741813cb2370edb4d8a0df4d46fea0c
suborno666 <subornodas251@gmail.com>
2024-10-22 16:44:38 +0530

    added header

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello',function(){
+    return response('<h1>Hello</h1>')
+    ->header('Content-type','text/plain')
+    ->header('foo','bar');
+});
+
+Route::get('/item/{id}',function($id){
+    return response("<h1>Item's id: {$id}</h1>");
+})->where('id','[0-9]+');
